6128305ada5cda5ddd6caa0e766f2a14b8c47f6a
PavelTurk <pavelturk2000@gmail.com>
2024-02-21 21:20:24 +0200

    Initial commit

---
+++ com.foo.impl1/src/test/java/com/foo/impl1/package1/ClassB1Test.java
@@ -0,0 +1,17 @@
+package com.foo.impl1.package1;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author Pavel
+ */
+public class ClassB1Test {
+
+    @Test
+    public void test() {
+        ClassB1 b1 = new ClassB1();
+        b1.getFf1();
+        b1.getFf10();
+    }
+}
---
+++ com.foo.impl1/src/test/java/module-info.java
@@ -0,0 +1,7 @@
+
+open module com.foo.impl1 {
+    requires org.slf4j;
+    requires com.foo.api;
+
+    requires org.junit.jupiter.api;
+}
